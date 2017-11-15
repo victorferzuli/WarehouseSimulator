bef5045287f54526ae5837180df88d4e31a19de1
VictorHdzF <victor_hferzuli@hotmail.com>
2017-11-15 12:45:32 -0600

    initial commit

---
+++ Assets/Scripts/ProductP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ProductP : MonoBehaviour {
+    public Text status;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*if (Input.GetKey(KeyCode.Plus))
+            status.text = "Products picked in zone: " + actualZone;
+        if (Input.GetKey(KeyCode.Minus))
+            status.text = "Products delivered in zone: " + actualZone;
+            */
+    }
+}
---
+++ Assets/Scripts/Zone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Zone : MonoBehaviour {
+    public Material[] material;
+    Renderer rend;
+    string actualZone;
+    public Text zone;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        rend.sharedMaterial = material[0];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rend.sharedMaterial = material[1];
+        actualZone = this.gameObject.name;
+        zone.text = "Current zone: "+ actualZone;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
